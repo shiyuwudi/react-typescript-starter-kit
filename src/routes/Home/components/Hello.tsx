ceb099fdbfb7dbf924f5e5fda5eac55394ceeb4f
lixiaoyang <lixiaoyang2345@gmail.com>
2016-10-29 11:51:54 +0800

    antd支持TypeScript2.0

--- src/routes/Home/components/Hello.tsx
+++ src/routes/Home/components/Hello.tsx
@@ -2,12 +2,13 @@
  * Created by lixiaoyang on 2016/10/27.
  */
 import * as React from "react";
+import {Button} from 'antd';
 
 export interface HelloProps { compiler: string; framework: string;
 }
 
 export class Hello extends React.Component<HelloProps, {}> {
     render() {
-        return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
+        return <h1><Button type="primary">删除</Button>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
 }
