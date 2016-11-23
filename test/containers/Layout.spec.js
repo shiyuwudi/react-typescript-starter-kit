9f8ccb32f8c9a8e98965819ff7e1d086c3c1623e
53U89WFDGHCW4FU\Administrator <885527712pan@163.com>
2016-11-23 15:15:25 +0800

    添加一部分测试

--- test/containers/Layout.spec.js
+++ test/containers/Layout.spec.js
@@ -1,17 +1,46 @@
 import React from 'react';
 import {expect} from 'chai';
-import {shallow} from 'enzyme';
+import {shallow,mount} from 'enzyme';
 import Layout from '../../src/layouts/CoreLayout/CoreLayout';
 
 describe('(layout) Core', () => {
-  const wrapper = shallow(<Layout />);
+  const wrapper = mount(<Layout />);
 
   it('renders as a <div>', () => {
     expect(wrapper.type()).to.eql('div');
   });
 
+  it('循环测试左侧导航划过变变亮', () => {
+    let wrapper = mount(<Layout />);
+    wrapper.find('.ant-menu-item').forEach(function (node) {
+      node.simulate('mouseenter');
+      expect(node.hasClass('ant-menu-item-active')).to.equal(true);
+    });
+  });
+
+  it('循环测试左侧导航点击变蓝', () => {
+    let wrapper = mount(<Layout />);
+    wrapper.find('.ant-menu-item').forEach(function (node) {
+      node.simulate('click');
+      expect(node.hasClass('ant-menu-item-selected')).to.equal(true);
+    });
+  });
+
+  it('循环测试左侧导航点击与hover', () => {
+    let wrapper = mount(<Layout />);
+    wrapper.find('.ant-menu-item').forEach(function (node) {
+
+        node.simulate('click');
+        expect(node.hasClass('ant-menu-item-selected')).to.equal(true);
+
+        node.simulate('mouseenter');
+        expect(node.hasClass('ant-menu-item-active')).to.equal(true);
+
+    });
+  });
+
   it('菜单默认关闭', () => {
-    expect(wrapper.find('.ant-layout-aside.ant-layout-aside-collapse')).to.have.length(1);
+    expect(wrapper.find('.ant-layout-aside-collapse')).to.have.length(1);
   });
 
   it('5个导航菜单', () => {
@@ -20,10 +49,13 @@ describe('(layout) Core', () => {
 
   it('菜单打开1', () => {
     wrapper.find('.ant-aside-action').simulate('click');
-    expect(wrapper.find('.ant-layout-aside.ant-layout-aside-collapse')).to.have.length(0);
+    expect(wrapper.find('.ant-layout-aside-collapse')).to.have.length(0);
   });
   it('菜单打开2', () => {
     wrapper.find('.ant-aside-action').simulate('click');
     expect(wrapper.find('.ant-layout-aside')).to.have.length(1);
   });
+
+
+
 });
---
+++ test/containers/list.spec.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Administrator on 2016/11/23.
+ */
+import React from 'react';
+import {expect} from 'chai';
+import {mount} from 'enzyme';
+import List from '../../src/routes/List/components/List.tsx'
+
+describe('test list',()=>{
+    //var wrapper = mount(<List items={items} />);
+    //it('data load success',()=>{
+    //    expect(wrapper.find('.ant-table-tbody').children()).to.equal(1);
+    //});
+});
